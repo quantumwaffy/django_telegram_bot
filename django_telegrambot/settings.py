3783e8c8119c8ec8b2bc7389a3c309fe66f29e12
django <sashchystyh@yandex.ru>
2021-09-21 01:00:05 +0300

    refactor: added local settings

--- django_telegrambot/settings.py
+++ django_telegrambot/settings.py
@@ -73,12 +73,7 @@ WSGI_APPLICATION = "django_telegrambot.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = "Your database settings"
 
 
 # Password validation
