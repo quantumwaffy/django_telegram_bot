058dd6331871c3d4bd8d7c96b23624377df9b98f
django <sashchystyh@yandex.ru>
2021-09-23 00:53:23 +0300

    feat: app for telegram bot

--- django_telegrambot/settings.py
+++ django_telegrambot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "telegram_bot",
 ]
 
 MIDDLEWARE = [
@@ -120,4 +121,7 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 if DEBUG:
-    from .local_settings import *
+    try:
+        from .local_settings import *
+    except ImportError:
+        pass
